2d7a0371023adb76a70ef74ff727713564719707
Thabang Maphangisane <thabangh647@gmail.com>
2024-07-30 15:39:33 +0200

    4. Force locale with URL parameter

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -44,7 +44,7 @@ def index():
     """
     Route to render the index page.
     """
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == "__main__":
